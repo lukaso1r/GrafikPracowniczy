489ff6b63b08fffeb9b6a48589ebe1330769f19c
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2023-12-03 15:21:27 +0100

    nie dupło

--- src/app/login-manager/login-manager.component.ts
+++ src/app/login-manager/login-manager.component.ts
@@ -7,26 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginManagerComponent implements OnInit{
 
-  loginStatus: boolean = false;
-  showWorkerListStatus: boolean = false;
-  showAddWorkerStatus: boolean = false;
-  showCalencdarStatus: boolean = false;
+  loginStatus: boolean;
+  showWorkerListStatus: boolean;
+  showAddWorkerStatus: boolean;
+  showCalencdarStatus: boolean;
 
-  outPutActiveList: boolean[] = [
-    this.showWorkerListStatus,
-    this.showAddWorkerStatus,
-    this.showCalencdarStatus
-  ];
+  outPutActiveList: boolean[];
 
   constructor(){
+
     this.loginStatus = false;
+    this.showWorkerListStatus = false;
+    this.showAddWorkerStatus = false;
+    this.showCalencdarStatus = false;
+
+    this.outPutActiveList = [
+      this.showWorkerListStatus,
+      this.showAddWorkerStatus,
+      this.showCalencdarStatus
+    ];
   }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+
     this.loginStatus = false;
     this.showWorkerListStatus = false;
+    this.showAddWorkerStatus = false;
+    this.showCalencdarStatus = false;
+
+    this.outPutActiveList = [
+      this.showWorkerListStatus,
+      this.showAddWorkerStatus,
+      this.showCalencdarStatus
+    ];
 
   }
 
@@ -38,8 +53,23 @@ export class LoginManagerComponent implements OnInit{
     this.showWorkerListStatus = true;
   }
 
-  changeOutPutActiveList(){
-    console.log("haha");
+  changeOutPutActiveList(buttonId: string){
+    //ustawienie wszystkich elementów jako off
+    this.outPutActiveList = this.outPutActiveList.map(() => false);
+
+    // ustawienie elementu pobranego z przycisku na true
+    const index = this.outPutActiveList.findIndex((element, idx) => idx === +buttonId);
+    if (index !== -1) {
+      this.outPutActiveList[index] = true;
+    }
+    console.log(this.outPutActiveList)
+
+  }
+
+  wyglogowano(){
+    this.loginStatus = false;
+    //ustawienie wszystkich elementów jako off
+    this.outPutActiveList = this.outPutActiveList.map(() => false);
   }
 
 }
