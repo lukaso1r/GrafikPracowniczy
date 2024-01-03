18f503d2a51ccaa9f0524c7c429cec95a226d1ba
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2024-01-03 16:36:12 +0100

    naprawa XD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { FormsModule } from '@angular/forms';
 import { ShiftsViewComponent } from './shifts-view/shifts-view.component';
 import { MouseDirective } from './dyrektywy/mouse.directive';
 import { WorkerShiftsComponent } from './worker-shifts/worker-shifts.component';
-import { MesseageMenuComponent } from './messenger/messeage-menu/messeage-menu.component';
 
 
 @NgModule({
@@ -30,8 +29,7 @@ import { MesseageMenuComponent } from './messenger/messeage-menu/messeage-menu.c
     WorkerSearchPipe,
     ShiftsViewComponent,
     MouseDirective,
-    WorkerShiftsComponent,
-    MesseageMenuComponent
+    WorkerShiftsComponent
   ],
   imports: [
     BrowserModule,
